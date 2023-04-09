03c073273fa690a4e5bc9cf1b338a1cc95ce34ea
Alisa <alisakovalhome@mail.ru>
2023-04-09 15:18:30 +0300

    Add lenear-search solutions

--- linear-search.js
+++ linear-search.js
@@ -3,13 +3,67 @@
 // в котором объект был изначально найден. Если объект не найден, то функция возвращает значение -1.
 function linearSearch(n, arr) {
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === n) {
+      return i
+    }
+  }
+  return -1
+
+  //рекурсия, с этим доразобраться
+  // let result = []
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(result);
+  //   if (Array.isArray(arr[i])) {
+  //     linearSearch(n, arr[i])
+  //   } else {
+
+  //     result.push(arr[i])
+  //     // console.log(result);
+  //   }
+
+  // }
+  // return result
+
+  //  while
+  // let i = 0
+  // while (i < arr.length) {
+  //   if (arr[i] === n) {
+  //     return i
+  //   }
+  //   i++;
+  // }
+  // return -1
 }
+
+// console.log(linearSearch(3, [1, [2, 3, 4, 5]]));
 
 
 //напишем новый метод globalLinearSearch, который возвращает массив всех индексов, в которых найден элемент.
 function globalLinearSearch(n, arr) {
 
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === n) {
+      newArr.push(i)
+    }
+  }
+  return newArr
+
+  //while
+  // let newArr = []
+  // let i = 0
+  // while (i < arr.length) {
+  //   if (arr[i] === n) {
+  //     newArr.push(i)
+  //   }
+  //   i++
+  // }
+  // return newArr
+
 }
+
+// console.log(globalLinearSearch('a', ['b', 'a', 'n', 'a', 'n', 'a', 's']));
 
 module.exports = {
   linearSearch,
--- spec/linear-search.spec.js
+++ spec/linear-search.spec.js
@@ -1,17 +1,39 @@
 const { linearSearch, globalLinearSearch } = require('../linear-search');
 
-describe("linearSearch()", function() {
-  it("returns first index of item in array", function() {
+describe("linearSearch()", function () {
+  it("returns first index of item in array", function () {
     let item = 3;
-    let arr = [1,2,3,4,5];
+    let arr = [1, 2, 3, 4, 5];
     expect(linearSearch(item, arr)).toEqual(2);
+  });
+  it("returns first index of item in array with string items", function () {
+    let item = 'hi';
+    let arr = ['bye', 'hello', 'how a u', 'hi'];
+    expect(linearSearch(item, arr)).toEqual(3);
+  });
+  it("returns -1 if there is no item in array or there is empty array", function () {
+    let item = 9;
+    let arr = [1, 2, 3, 4, 5];
+    expect(linearSearch(item, arr)).toEqual(-1);
+    expect(linearSearch(item, [])).toEqual(-1);
+  });
+  it("returns true of false after comparing the founded index with test ndex", function () {
+    let item = 9;
+    let arr = [5, 2, 7, 9, 3];
+    expect(linearSearch(item, arr)).toBeGreaterThan(2);
+    expect(linearSearch(item, arr)).toBeLessThanOrEqual(4);
+    expect(linearSearch(item, arr)).toBeLessThanOrEqual(3);
   });
 });
 
-describe("globalLinearSearch()", function() {
-  it("returns array indxes of items in array", function() {
+describe("globalLinearSearch()", function () {
+  it("returns array indexes of items in array", function () {
     let item = 'a';
     let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
     expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
+    expect(globalLinearSearch('i', ['h', 'i'])).toEqual([1]);
+    expect(globalLinearSearch('i', arr)).toEqual([]);
+    expect(globalLinearSearch(item, 'bananas')).toEqual([1, 3, 5]);
+    expect(globalLinearSearch(3, [3, 2, 3, 4, 5])).toEqual([0, 2]);
   });
 });
